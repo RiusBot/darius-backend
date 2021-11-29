2ddb2869fb27407e89c7f5c63b75b42670e3c52e
dada <bb04902103@gmail.com>
2021-11-29 13:24:30 +0000

    add api upadte

--- main/src/controllers/v1/api.py
+++ main/src/controllers/v1/api.py
@@ -1,6 +1,6 @@
 import logging
 from aiohttp.web import json_response
-from main.src.core.api import _get_user_api, _create_user_api, _delete_user_api
+from main.src.core.api import _get_user_api, _create_user_api, _delete_user_api, _update_user_api
 from main.src.core.auth import authenticate
 
 
@@ -62,6 +62,40 @@ async def create_user_api(request):
         )
     except Exception as e:
         logger.error("Create api error.")
+        logger.exception("")
+        return json_response(
+            status=500,
+            data={
+                'code': 500,
+                'message': str(e)
+            }
+        )
+    
+    
+async def update_user_api(request):
+
+    json_payload = await request.json()
+    if authenticate(json_payload) is False:
+        logger.error("access token is not valid")
+        response_data = {
+            "error_message": "access token is not valid"
+        }
+        return json_response(data=response_data, status=401)
+
+    try:
+        logger.info("Update user api")
+        user_id = json_payload["user_id"]
+        api_id = json_payload["api_id"]
+        api_key = json_payload["api_key"]
+        api_secret = json_payload["api_secret"]
+        exchange = json_payload["exchange"]
+        api_id = await _update_user_api(user_id, api_id, api_key, api_secret, exchange)
+        return json_response(
+            status=200,
+            data={}
+        )
+    except Exception as e:
+        logger.error("Update api error.")
         logger.exception("")
         return json_response(
             status=500,
--- main/src/core/api.py
+++ main/src/core/api.py
@@ -13,11 +13,14 @@ logger = logging.getLogger(__name__)
 @atomic()
 async def _get_user_api(user_id: int) -> List[Api]:
     logger.info(f"Get api for user {user_id}")
+    user = await User.filter(id=user_id).first()
+    if user is None:
+        raise Excetion("Invalid user_id")
+
     Api_Pydantic_List = pydantic_queryset_creator(
         Api,
         include=["api_key", "api_secret", "exchange", "id"]
     )
-    user = await User.filter(id=user_id).first()
     api_list = await Api_Pydantic_List.from_queryset(user.api_user.filter(is_del=False).all())
     api_list = json.loads(api_list.json())
     for api in api_list:
@@ -26,7 +29,7 @@ async def _get_user_api(user_id: int) -> List[Api]:
     return api_list
 
 
-def validate_api(api_key: str, api_secret: str, exchange: str):
+def validate_api_permission(api_key: str, api_secret: str, exchange: str):
     exchange = getattr(ccxt, exchange)({
         'enableRateLimit': True,
         'apiKey': api_key,
@@ -44,6 +47,7 @@ def validate_api(api_key: str, api_secret: str, exchange: str):
 async def _create_user_api(user_id: int, api_key: str, api_secret: str, exchange: str) -> int:
     logger.info(f"Create new api for user [{user_id}]")
 
+    # validate user
     user = await User.filter(id=user_id).filter(is_del=False).first()
     if user is None:
         raise Exception("Invalid user_id")
@@ -53,8 +57,8 @@ async def _create_user_api(user_id: int, api_key: str, api_secret: str, exchange
     if api_list and len(api_list) > 3:
         raise Exception("Maximum 3 api per user")
 
-    # validate api
-    validate_api(api_key, api_secret, exchange)
+    # validate api permission
+    validate_api_permission(api_key, api_secret, exchange)
 
     # create api
     api = Api.create(
@@ -67,6 +71,30 @@ async def _create_user_api(user_id: int, api_key: str, api_secret: str, exchange
 
     logger.info(f"Create api [{api_id}]")
     return api_id
+
+
+@atomic()
+async def _update_user_api(user_id: int, api_id: int, api_key: str, api_secret: str, exchange: str) -> int:
+    logger.info(f"Update api [{api_id}] for user [{user_id}]")
+    
+    # validate user
+    user = await User.filter(id=user_id).filter(is_del=False).first()
+    if user is None:
+        raise Exception("Invalid user_id")
+
+    # validate api
+    api = await user.api_user.filter(is_del=False).filter(id=api_id).first()
+    if api is None:
+        raise Exception("Invalid api.")
+        
+    # validate api permission
+    validate_api_permission(api_key, api_secret, exchange)
+
+    # update api
+    api.api_key = api_key
+    api.api_secret = api_secret
+    api.exchange = exchange
+    await api.save()
 
 
 @atomic()
