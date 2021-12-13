4f8d8cb8c93e66b25eb402d143513d609584d7e0
dada <bb04902103@gmail.com>
2021-12-13 09:22:33 +0000

    validate if api is still in use when delete.

--- main/src/core/api.py
+++ main/src/core/api.py
@@ -137,6 +137,9 @@ async def _delete_user_api(uid: str, api_id: int) -> int:
         raise BackendException("Invalid api_id")
 
     # validate no bot using
+    bot_using_this_api = await API.config_api.filter(id=api_id).filter(is_del=False).first()
+    if bot_using_this_api is not None:
+        raise BackendException("API still in use.")
 
     # delete api
     api.is_del = True
