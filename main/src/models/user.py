5179a59ca598df0ebf1cb352f3b438bd5096216b
rayray625 <ray85625@gmail.com>
2022-01-13 20:50:11 +0800

    Fix null referrer_count when create user

--- main/src/models/user.py
+++ main/src/models/user.py
@@ -14,7 +14,7 @@ class User(BaseModel):
     balance = fields.FloatField(null=False, default=0.0)
     referrer = fields.CharField(8, null=True)
     referral_code = fields.CharField(8, null=False, unique=True)
-    referrer_count = fields.IntField(0, null=False)
+    referrer_count = fields.IntField(null=False, default=0)
 
     class Meta:
         table = "user"
