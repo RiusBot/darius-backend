45262f049d24bb5a2a23c419897bb5f7b14531f2
rayray625 <ray85625@gmail.com>
2022-02-22 23:25:41 +0800

    remove unused field

--- main/src/models/user.py
+++ main/src/models/user.py
@@ -8,7 +8,6 @@ class User(BaseModel):
 
     user_name = fields.CharField(16, null=True)
     email = fields.CharField(32, null=True, unique=True)
-    password = fields.CharField(50, null=True)
     role = fields.ForeignKeyField("darius.Role", related_name="user_role")
     uid = fields.CharField(28, null=False)
     balance = fields.FloatField(null=False, default=0.0)
