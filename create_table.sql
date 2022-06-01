1e195c4617cab943f7d4b5f8c2fe21cc6c2e74e2
dada <bb04902103@gmail.com>
2022-06-01 14:34:29 +0000

    add updated_at, orm no change yet

--- create_table.sql
+++ create_table.sql
@@ -2,9 +2,12 @@
 CREATE TABLE `permission` (
     `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `role_id` int(11),
     `service` varchar(50) NOT NULL,
+    `is_del` BOOLEAN NOT NULL DEFAULT False,
     KEY (`role_id`),
+    KEY (`is_del`),
     CONSTRAINT FOREIGN KEY(`role_id`) REFERENCES `role`(id) ON DELETE CASCADE
 ) CHARACTER SET utf8;
 
@@ -12,8 +15,11 @@ CREATE TABLE `permission` (
 CREATE TABLE `role` (
     `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `name` varchar(50) UNIQUE NOT NULL,
-    KEY (`name`)
+    `is_del` BOOLEAN NOT NULL DEFAULT False,
+    KEY (`name`),
+    KEY (`is_del`)
 ) CHARACTER SET utf8;
 
 
@@ -21,6 +27,7 @@ CREATE TABLE `role` (
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   `user_name` varchar(16),
   `email` varchar(32) UNIQUE,
   `uid` varchar(28) UNIQUE NOT NULL,
@@ -29,12 +36,14 @@ CREATE TABLE `user` (
   `referrer` varchar(8),
   `referral_code` varchar(8) UNIQUE NOT NULL,
   `referrer_count` int(11) NOT NULL  DEFAULT 0,
+  `is_del` BOOLEAN NOT NULL DEFAULT False,
    KEY (`user_name`),
    KEY (`email`),
    KEY (`created_at`),
    KEY (`uid`),
    KEY (`referrer`),
    KEY (`referral_code`),
+   KEY (`is_del`),
    CONSTRAINT FOREIGN KEY (`role_id`) REFERENCES `role`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -43,11 +52,14 @@ CREATE TABLE `user` (
 CREATE TABLE IF NOT EXISTS `telegram` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `created_at` DATETIME(6)   DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `telegram_id` INT UNIQUE NOT NULL,
     `user_id` INT UNIQUE NOT NULL,
+    `is_del` BOOLEAN NOT NULL DEFAULT False,
     KEY (`user_id`),
     KEY (`telegram_id`),
     KEY (`created_at`),
+    KEY (`is_del`),
     FOREIGN KEY(user_id) REFERENCES user(id)
 ) CHARACTER SET utf8;
 
@@ -55,13 +67,16 @@ CREATE TABLE IF NOT EXISTS `telegram` (
 CREATE TABLE `plan` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   `price` decimal(19, 4) NOT NULL,
   `day` decimal(10, 0) NOT NULL, -- should modify the term, available_day
   `name` varchar(50) UNIQUE NOT NULL,
   `channel` varchar(50) NOT NULL,
+  `is_del` BOOLEAN NOT NULL DEFAULT False,
   KEY (`name`),
   KEY (`created_at`),
   KEY (`channel`),
+  KEY (`is_del`),
   CHECK(day >= 0),
   CHECK(price >= 0)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -70,15 +85,18 @@ CREATE TABLE `plan` (
 CREATE TABLE `subscription` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   `user_id` int(11) NOT NULL,
   `plan_id` int(11) NOT NULL,
   `expire_date` DATETIME(6),
   `channel` varchar(50) NOT NULL,
   `invite_link` varchar(50) UNIQUE,
+  `is_del` BOOLEAN DEFAULT False,
   KEY (`user_id`),
   KEY (`plan_id`),
   KEY (`expire_date`),
   KEY (`channel`),
+  KEY (`is_del`),
   UNIQUE KEY `user_channel` (`user_id`, `channel`,`is_del`),
   CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `user`(`id`),
   CONSTRAINT FOREIGN KEY (`plan_id`) REFERENCES `plan`(`id`)
@@ -88,15 +106,18 @@ CREATE TABLE `subscription` (
 CREATE TABLE `transaction` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+  `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
   `user_id` int(11) NOT NULL,
   `wallet` varchar(128) NOT NULL,
   `txid` varchar(128) UNIQUE NOT NULL,
   `date` DATETIME(6) NOT NULL,
   `amount` DOUBLE NOT NULL,
+  `is_del` BOOLEAN NOT NULL DEFAULT False,
   KEY (`user_id`),
   KEY (`txid`),
   KEY (`wallet`),
   KEY (`date`),
+  KEY (`is_del`),
   CONSTRAINT FOREIGN KEY (`user_id`) REFERENCES `user`(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -104,13 +125,16 @@ CREATE TABLE `transaction` (
 CREATE TABLE `api` (
     `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `created_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `user_id` int(11) NOT NULL,
     `api_key` varchar(64) NOT NULL,
     `api_secret` varchar(400) NOT NULL,
     `exchange` varchar(16) NOT NULL,
     `subaccount` varchar(32),
+    `is_del` BOOLEAN NOT NULL DEFAULT False,
     KEY (`user_id`),
     KEY (`exchange`),
+    KEY (`is_del`),
     UNIQUE KEY `api_cred` (`api_key`,`api_secret`),
     CONSTRAINT FOREIGN KEY(`user_id`) REFERENCES `user`(id)
 ) CHARACTER SET utf8;
@@ -124,9 +148,11 @@ CREATE TABLE `pair` (
     `name` varchar(64) NOT NULL,
     `types` varchar(16) NOT NULL,
     `lists` LONGTEXT NOT NULL,
+    `is_del` BOOLEAN NOT NULL DEFAULT False,
     KEY (`user_id`),
     KEY (`type`),
     KEY (`name`),
+    KEY (`is_del`),
     UNIQUE KEY `user_name` (`user_id`,`name`),
     CONSTRAINT FOREIGN KEY(`user_id`) REFERENCES `user`(id)
 ) CHARACTER SET utf8;
@@ -135,18 +161,21 @@ CREATE TABLE `pair` (
 CREATE TABLE IF NOT EXISTS `bot_order` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `created_at` DATETIME(6)   DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `channel` VARCHAR(32) NOT NULL,
     `status` VARCHAR(16) NOT NULL,
     `config_id` INT NOT NULL,
     `user_id` INT NOT NULL,
     `is_trial` BOOLEAN  DEFAULT FALSE  NOT NULL,
     `trial_expired_at` DATETIME(6)   DEFAULT CURRENT_TIMESTAMP(6),
+    `is_del` BOOLEAN NOT NULL DEFAULT False,
     KEY (`user_id`),
     KEY (`status`),
     KEY (`channel`),
     KEY (`created_at`),
     KEY (`config_id`),
     KEY (`is_trial`),
+    KEY (`is_del`),
     KEY (`trial_expired_at`),
     FOREIGN KEY(user_id) REFERENCES user(id)
 ) CHARACTER SET utf8;
@@ -159,6 +188,7 @@ CREATE TABLE IF NOT EXISTS `bot_config` (
     `api_id` INT NOT NULL,
     `pair_id` INT,
     `created_at` DATETIME(6)   DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `test` BOOLEAN  DEFAULT FALSE  NOT NULL,
     `hyperopt` BOOLEAN  DEFAULT FALSE  NOT NULL,
     `target` VARCHAR(16) NOT NULL,
@@ -173,9 +203,11 @@ CREATE TABLE IF NOT EXISTS `bot_config` (
     `duplicate` BOOLEAN DEFAULT FALSE,
     `minimum_volume` DOUBLE,
     `others` VARCHAR(64),
+    `is_del` BOOLEAN NOT NULL DEFAULT False,
     KEY (`bot_id`),
     KEY (`api_id`),
     KEY (`pair_id`),
+    KEY (`is_del`),
     CHECK(quantity >= 50),
     CHECK(leverage > 0),
     CHECK(margin > 0),
@@ -193,15 +225,18 @@ ALTER TABLE `bot_order` ADD CONSTRAINT FOREIGN KEY(`config_id`) REFERENCES `bot_
 CREATE TABLE IF NOT EXISTS `hyperopt` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `created_at` DATETIME(6) NOT NULL  DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `start_at` DATETIME(6) NOT NULL,
     `end_at` DATETIME(6) NOT NULL,
     `channel` VARCHAR(32) NOT NULL,
     `params` VARCHAR(4096) NOT NULL,
     `days` INT NOT NULL,
     `loss` VARCHAR(32) NOT NULL,
+    `is_del` BOOLEAN NOT NULL DEFAULT False,
     KEY (`channel`),
     KEY (`end_at`),
     KEY (`start_at`),
+    KEY (`is_del`),
     KEY (`loss`)
 ) CHARACTER SET utf8;
 
@@ -209,22 +244,26 @@ CREATE TABLE IF NOT EXISTS `hyperopt` (
 CREATE TABLE IF NOT EXISTS `performance` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `created_at` DATETIME(6) NOT NULL  DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `channel` VARCHAR(32) NOT NULL,
     `start_at` DATETIME(6) NOT NULL,
     `end_at` DATETIME(6) NOT NULL,
     `result` LONGTEXT NOT NULL,
     `hyperopt_id` INT NOT NULL,
+    `is_del` BOOLEAN NOT NULL DEFAULT False,
     FOREIGN KEY(hyperopt_id) REFERENCES hyperopt(id),
     KEY (`channel`),
     KEY (`created_at`),
     KEY (`start_at`),
     KEY (`end_at`),
+    KEY (`is_del`),
 ) CHARACTER SET utf8;
 
 -- message
 CREATE TABLE IF NOT EXISTS `message` (
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `created_at` DATETIME(6)   DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `message_timestamp` DATETIME(6),
     `recieve_timestamp` DATETIME(6),
     `channel` VARCHAR(32) NOT NULL,
@@ -234,13 +273,15 @@ CREATE TABLE IF NOT EXISTS `message` (
     `entry` DOUBLE,
     `stop_loss` DOUBLE,
     `take_profit` DOUBLE,
+    `is_del` BOOLEAN NOT NULL DEFAULT False,
     CHECK(entry > 0),
     CHECK(stop_loss > 0),
     CHECK(take_profit > 0),
     KEY (`created_at`),
     KEY (`channel`),
     KEY (`symbol`),
-    KEY (`action`)
+    KEY (`action`),
+    KEY (`is_del`)
 ) CHARACTER SET utf8;
 
 -- trade history
@@ -249,31 +290,18 @@ CREATE TABLE IF NOT EXISTS `trade_history` (
     `bot_id` INT NOT NULL,
     `message_id` INT NOT NULL,
     `created_at` DATETIME(6)   DEFAULT CURRENT_TIMESTAMP(6),
+    `updated_at` DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6),
     `status` VARCHAR(16) NOT NULL,
     `error` VARCHAR(1024),
     `open_order` VARCHAR(50),
     `sl_order` VARCHAR(50),
     `tp_order` VARCHAR(50),
+    `is_del` BOOLEAN NOT NULL DEFAULT False,
     KEY (`bot_id`),
     KEY (`message_id`),
     KEY (`created_at`),
     KEY (`status`),
+    KEY (`is_del`),
     FOREIGN KEY(bot_id) REFERENCES bot_order(id),
     FOREIGN KEY(message_id) REFERENCES message(id)
 ) CHARACTER SET utf8;
-
-ALTER TABLE role add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE permission add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE subscription add COLUMN is_del BOOLEAN DEFAULT False;
-ALTER TABLE plan add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE user add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE api add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE bot_order add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE bot_config add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE trade_history add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE message add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE transaction add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE telegram add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE performance add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE hyperopt add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
-ALTER TABLE pair add COLUMN is_del BOOLEAN NOT NULL DEFAULT False;
